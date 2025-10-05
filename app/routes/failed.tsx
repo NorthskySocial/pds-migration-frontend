151fde524c70fb9e25613c51c63a5de6a57e6973
Ændra Rininsland <aendra@hairysquid.com>
2025-10-05 13:47:11 +0100

    Various fixes to allow resuming journey

--- app/routes/failed.tsx
+++ app/routes/failed.tsx
@@ -1,6 +1,7 @@
 import { redirect, data, useFetcher } from "react-router";
 import { getSession, commitSession } from "../sessions.server";
 import { logger } from "~/util/logger";
+import type { Route } from "./+types";
 
 export async function loader({ request }: Route.LoaderArgs) {
   const session = await getSession(request.headers.get("Cookie"));
