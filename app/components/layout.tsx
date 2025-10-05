61b523435e69e15e1f52fad439a9a5681506fdad
CarlM <nic.chop@gmail.com>
2025-10-05 14:10:43 -0400

    UI Kludge
    Kludged the UI to add some visual interest.

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,16 +1,19 @@
 import { Provider } from "@/components/ui/provider";
 import { Container, Image, VStack } from "@chakra-ui/react";
 import logo from "../assets/Northsky-IconCentered-Color.png";
+import nsBackground from "../assets/Northsky-Background.jpg";
+
+
 
 export const Layout = ({ children }) => (
   <Provider>
-    <Container maxWidth={"4xl"}>
+    <Container maxWidth={"4xl"} bgColor={"rgb(31, 11, 53)"} >
       <VStack
         margin="0 auto"
         width={"3xl"}
         height={"100vh"}
-        
         alignItems={"center"}
+        
       >
         <VStack mb="5">
           <Image height={"150px"} src={logo} alt="Northsky" />
