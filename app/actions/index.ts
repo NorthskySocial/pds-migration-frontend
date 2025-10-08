4d8687c72edac1f40da70b1eeb6b58d49c0b1850
CarlM <nic.chop@gmail.com>
2025-10-08 19:57:35 -0400

    Fix to #17 take 2
    Corrected some bad references.

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -128,122 +128,120 @@ export async function createDestAccount(
   const handle = ((data.get("handle") as string) ?? "").toLowerCase();
   const submitted = data.has("submit");
   const dest_hostname = new URL(pds_dest!).host;
-  const handle_dest = `${handle}.${
-    dest_hostname.match("localhost") ? "test" : dest_hostname
-  }`;
+  const handle_dest = `${handle}.${dest_hostname.match("localhost") ? "test" : dest_hostname
+    }`;
   const org_hostname = new URL(pds_origin!).host;
-  const handle_org = `${handle}.${
-    org_hostname.match("localhost") ? "test" : org_hostname
-  }`;
+  const handle_org = `${handle_origin}.${org_hostname.match("localhost") ? "test" : org_hostname
+    }`;
   const pw_org = password_origin as string;
 
-// Check passwords matching
-if (pw_dest !== pwConfirm && pw_dest.length && pwConfirm.length) {
-  throw new PasswordValidationError("Passwords do not match");
-}
-
-// Check password length
-if (pw_dest?.length < 8 && pw_dest.length > 0) {
-  throw new PasswordValidationError("Password must be at least 8 characters");
-}
-
-// Check handle availability
-if (!handle.length) {
-  return { handle_available: null, token_dest: null };
-} else {
-  const handle_available = await f(
-    `${pds_dest}/xrpc/com.atproto.identity.resolveHandle?handle=${handle_dest}`
-  )
-    .then<{ message: string; error: string } & { did: string }>((r) =>
-      r.json()
-    )
-    .then((d) => d.message === "Unable to resolve handle" || d.did === did);
-
-  if (!submitted) return { handle_available, handle_dest };
-
-  // Create account directly if service token is not available
-  // This is a new, non migrated account
-  if (!did) {
-
-    // Get new user token
-    const agent_dest = new AtpAgent({
-      service: pds_dest!,
-      fetch: f as typeof fetch,
-    });
-    const response = await agent_dest.createAccount({
-      email: email,
-      handle: handle_dest,
-      inviteCode: inviteCode,
-      password: pw_dest,
-    });
-
-    if (!response.success) {
-      logger.error(response.data);
-      throw new CreateAccountError("error creating account");
-    }
-
-    return { token_dest: response.data.accessJwt };
-  } else {
-    //This is a migrated account
-
-    // Login to origin PDS
-
-    const origin_agent = new AtpAgent({
-      service: org_hostname,
-      fetch: f as typeof fetch,
-    });
-
-    const { data: agentSessionData } = await origin_agent.login({
-      identifier: handle_org,
-      password: pw_org,
-    });
-
-    const body = {
-      pds_host: pds_dest,
-      handle: handle_dest,
-      token: token_service,
-      password: pw_dest,
-      email,
-      did,
-      invite_code: inviteCode,
-      recovery_key: user_recover_key,
-    };
-
-
-
-    const createAccountRes = await f(`${MIGRATOR_BACKEND}/create-account`, {
-      method: "post",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(body),
-    });
-
-    logger.debug("create account debugging", body, {
-      headers: createAccountRes.headers,
-      body: createAccountRes.body,
-      ok: createAccountRes.ok,
-      status: createAccountRes.status,
-      statusText: createAccountRes.statusText,
-      url: createAccountRes.url,
-    });
-
-    // if (!createAccountRes.ok) {
-    //   throw new CreateAccountError(createAccountRes.statusText);
-    // }
-
-    // Get new user token
-    const agent_dest = new AtpAgent({
-      service: pds_dest!,
-      fetch: f as typeof fetch,
-    });
-
-    const { data } = await agent_dest.login({
-      identifier: handle_dest,
-      password: pw_dest,
-    });
-
-    return { token_dest: data.accessJwt };
+  // Check passwords matching
+  if (pw_dest !== pwConfirm && pw_dest.length && pwConfirm.length) {
+    throw new PasswordValidationError("Passwords do not match");
   }
-}
+
+  // Check password length
+  if (pw_dest?.length < 8 && pw_dest.length > 0) {
+    throw new PasswordValidationError("Password must be at least 8 characters");
+  }
+
+  // Check handle availability
+  if (!handle.length) {
+    return { handle_available: null, token_dest: null };
+  } else {
+    const handle_available = await f(
+      `${pds_dest}/xrpc/com.atproto.identity.resolveHandle?handle=${handle_dest}`
+    )
+      .then<{ message: string; error: string } & { did: string }>((r) =>
+        r.json()
+      )
+      .then((d) => d.message === "Unable to resolve handle" || d.did === did);
+
+    if (!submitted) return { handle_available, handle_dest };
+
+    // Create account directly if service token is not available
+    // This is a new, non migrated account
+    if (!did) {
+
+      // Get new user token
+      const agent_dest = new AtpAgent({
+        service: pds_dest!,
+        fetch: f as typeof fetch,
+      });
+      const response = await agent_dest.createAccount({
+        email: email,
+        handle: handle_dest,
+        inviteCode: inviteCode,
+        password: pw_dest,
+      });
+
+      if (!response.success) {
+        logger.error(response.data);
+        throw new CreateAccountError("error creating account");
+      }
+
+      return { token_dest: response.data.accessJwt };
+    } else {
+      //This is a migrated account
+
+      // Login to origin PDS
+
+      const origin_agent = new AtpAgent({
+        service: org_hostname,
+        fetch: f as typeof fetch,
+      });
+
+      const { data: agentSessionData } = await origin_agent.login({
+        identifier: handle_org,
+        password: pw_org,
+      });
+
+      const body = {
+        pds_host: pds_dest,
+        handle: handle_dest,
+        token: token_service,
+        password: pw_dest,
+        email,
+        did,
+        invite_code: inviteCode,
+        recovery_key: user_recover_key,
+      };
+
+
+
+      const createAccountRes = await f(`${MIGRATOR_BACKEND}/create-account`, {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+
+      logger.debug("create account debugging", body, {
+        headers: createAccountRes.headers,
+        body: createAccountRes.body,
+        ok: createAccountRes.ok,
+        status: createAccountRes.status,
+        statusText: createAccountRes.statusText,
+        url: createAccountRes.url,
+      });
+
+      // if (!createAccountRes.ok) {
+      //   throw new CreateAccountError(createAccountRes.statusText);
+      // }
+
+      // Get new user token
+      const agent_dest = new AtpAgent({
+        service: pds_dest!,
+        fetch: f as typeof fetch,
+      });
+
+      const { data } = await agent_dest.login({
+        identifier: handle_dest,
+        password: pw_dest,
+      });
+
+      return { token_dest: data.accessJwt };
+    }
+  }
 }
 
 export async function exportRepo(
--- app/util/process-state.ts
+++ app/util/process-state.ts
@@ -40,6 +40,7 @@ export const processState = async (
     token_service: session.get("token_service"),
     plc_hostname: session.get("plc_hostname"),
     did: session.get("did"),
+    password_origin: session.get("password_origin"),
     inviteCode: session.get("inviteCode"),
     email: session.get("email"),
     user_recover_key: session.get("user_recover_key"),
@@ -61,162 +62,163 @@ export const processState = async (
 
   const isCancelling = data.get("cancel");
 
-  console.log("isCancelling: "+isCancelling)
+  console.log("isCancelling: " + isCancelling)
 
-if (isCancelling)
-{
-  //Reset all session variables
- session.set("do_journey",undefined);
- session.set("handle_origin",undefined);
- session.set("handle_dest",undefined);
- session.set("pds_dest",undefined);
- session.set("pds_origin",undefined);
- session.set("token_origin",undefined);
- session.set("token_dest",undefined);
- session.set("token_service",undefined);
- session.set("plc_hostname",undefined);
- session.set("did",undefined);
- session.set("inviteCode",undefined);
- session.set("email",undefined);
- session.set("user_recover_key",undefined);
+  if (isCancelling) {
+    //Reset all session variables
+    session.set("do_journey", undefined);
+    session.set("handle_origin", undefined);
+    session.set("handle_dest", undefined);
+    session.set("pds_dest", undefined);
+    session.set("pds_origin", undefined);
+    session.set("token_origin", undefined);
+    session.set("token_dest", undefined);
+    session.set("token_service", undefined);
+    session.set("plc_hostname", undefined);
+    session.set("did", undefined);
+    session.set("inviteCode", undefined);
+    session.set("email", undefined);
+    session.set("user_recover_key", undefined);
+    session.set("password_origin", undefined);
 
     // state flags
- session.set("hasBackup",false);
- session.set("exportedRepo",false);
- session.set("importedRepo",false);
- session.set("exportedBlobs",false);
- session.set("importedBlobs",false);
- session.set("migratedPrefs",false);
- session.set("requestedPlcToken",false);
- session.set("originDeactivated",false);
- session.set("destActivated",false);
- session.set("migratedPlc",false);
+    session.set("hasBackup", false);
+    session.set("exportedRepo", false);
+    session.set("importedRepo", false);
+    session.set("exportedBlobs", false);
+    session.set("importedBlobs", false);
+    session.set("migratedPrefs", false);
+    session.set("requestedPlcToken", false);
+    session.set("originDeactivated", false);
+    session.set("destActivated", false);
+    session.set("migratedPlc", false);
 
-}
+  }
 
-else 
-{
-  switch (stage) {
-    case STAGES.INVITE_CODE: {
-      const invite = data.get("invite-code") as string;
-      state.do_journey =
-        (data.get("create") as "create" | null) ||
-        (data.get("migrate") as "migrate" | null) ||
-        "create";
-      state.inviteCode = invite;
-      session.set("inviteCode", state.inviteCode);
-      session.set("do_journey", state.do_journey);
-      break;
-    }
-
-    case STAGES.BACKUP_NOTICE: {
-      state.hasBackup = data.get("confirm") === "on";
-      session.set("hasBackup", state.hasBackup);
-      break;
-    }
-
-    case STAGES.ORIGIN_PDS_LOGIN: {
-      const {
-        pds_origin,
-        email,
-        token_origin,
-        token_service,
-        handle_origin,
-        did,
-      } = await loginOrigin(state, data, env);
-
-      session.set("pds_origin", pds_origin);
-      session.set("email", email);
-      session.set("token_origin", token_origin);
-      session.set("token_service", token_service);
-      session.set("handle_origin", handle_origin);
-      session.set("did", did);
-      break;
-    }
-
-    case STAGES.CREATE_DEST_ACCOUNT: {
-      if (!state.email) {
-        state.email = data.get("email") as string;
-        session.set("email", state.email);
+  else {
+    switch (stage) {
+      case STAGES.INVITE_CODE: {
+        const invite = data.get("invite-code") as string;
+        state.do_journey =
+          (data.get("create") as "create" | null) ||
+          (data.get("migrate") as "migrate" | null) ||
+          "create";
+        state.inviteCode = invite;
+        session.set("inviteCode", state.inviteCode);
+        session.set("do_journey", state.do_journey);
+        break;
       }
 
-      const { handle_available, token_dest, handle_dest } =
-        await createDestAccount(state, data, env);
-
-      if (token_dest) {
-        session.set("token_dest", token_dest);
-      } else if (handle_available) {
-        session.set("handle_dest", handle_dest);
+      case STAGES.BACKUP_NOTICE: {
+        state.hasBackup = data.get("confirm") === "on";
+        session.set("hasBackup", state.hasBackup);
+        break;
       }
 
-      break;
-    }
+      case STAGES.ORIGIN_PDS_LOGIN: {
+        const {
+          pds_origin,
+          email,
+          token_origin,
+          token_service,
+          handle_origin,
+          did,
+          password_origin
+        } = await loginOrigin(state, data, env);
 
-    case STAGES.EXPORT_REPO_ORIGIN: {
-      const { ok } = await exportRepo(state, env);
-      if (ok) {
-        session.set("exportedRepo", ok);
+        session.set("pds_origin", pds_origin);
+        session.set("email", email);
+        session.set("token_origin", token_origin);
+        session.set("token_service", token_service);
+        session.set("handle_origin", handle_origin);
+        session.set("did", did);
+        session.set("password_origin", password_origin);
+        break;
       }
-      break;
-    }
 
-    case STAGES.IMPORT_REPO_DEST: {
-      const { ok } = await importRepo(state, env);
-      if (ok) {
-        session.set("importedRepo", ok);
-      }
-      break;
-    }
-    case STAGES.EXPORT_BLOBS_ORIGIN: {
-      const { ok } = await exportBlobs(state, env);
-      if (ok) {
-        session.set("exportedBlobs", ok);
-      }
-      break;
-    }
-    case STAGES.IMPORT_BLOBS_DEST: {
-      const { ok } = await uploadBlobs(state, env);
-      if (ok) {
-        session.set("importedBlobs", ok);
-      }
-      break;
-    }
-    case STAGES.MIGRATE_PREFERENCES: {
-      const { ok } = await migratePreferences(state, env);
-      if (ok) {
-        session.set("migratedPrefs", ok);
-      }
-      break;
-    }
-    case STAGES.GENERATE_RECOVERY_KEY: {
-      session.set(
-        "user_recover_key",
-        data.get("user_recover_key") as string | null
-      );
-      break;
-    }
+      case STAGES.CREATE_DEST_ACCOUNT: {
+        if (!state.email) {
+          state.email = data.get("email") as string;
+          session.set("email", state.email);
+        }
 
-    case STAGES.REQUEST_PLC: {
-      const { ok } = await requestPlcToken(state, env);
-      if (ok) {
-        session.set("requestedPlcToken", ok);
-      }
-      break;
-    }
+        const { handle_available, token_dest, handle_dest } =
+          await createDestAccount(state, data, env);
 
-    case STAGES.ACTIVATE_DEST:
-    case STAGES.DEACTIVATE_ORIGIN:
-    case STAGES.MIGRATE_PLC: {
-      const { ok } = await validatePlcToken(state, data, env);
-      if (ok) {
-        session.set("destActivated", ok);
-        session.set("originDeactivated", ok);
-        session.set("migratedPlc", ok);
+        if (token_dest) {
+          session.set("token_dest", token_dest);
+        } else if (handle_available) {
+          session.set("handle_dest", handle_dest);
+        }
+
+        break;
       }
-      break;
+
+      case STAGES.EXPORT_REPO_ORIGIN: {
+        const { ok } = await exportRepo(state, env);
+        if (ok) {
+          session.set("exportedRepo", ok);
+        }
+        break;
+      }
+
+      case STAGES.IMPORT_REPO_DEST: {
+        const { ok } = await importRepo(state, env);
+        if (ok) {
+          session.set("importedRepo", ok);
+        }
+        break;
+      }
+      case STAGES.EXPORT_BLOBS_ORIGIN: {
+        const { ok } = await exportBlobs(state, env);
+        if (ok) {
+          session.set("exportedBlobs", ok);
+        }
+        break;
+      }
+      case STAGES.IMPORT_BLOBS_DEST: {
+        const { ok } = await uploadBlobs(state, env);
+        if (ok) {
+          session.set("importedBlobs", ok);
+        }
+        break;
+      }
+      case STAGES.MIGRATE_PREFERENCES: {
+        const { ok } = await migratePreferences(state, env);
+        if (ok) {
+          session.set("migratedPrefs", ok);
+        }
+        break;
+      }
+      case STAGES.GENERATE_RECOVERY_KEY: {
+        session.set(
+          "user_recover_key",
+          data.get("user_recover_key") as string | null
+        );
+        break;
+      }
+
+      case STAGES.REQUEST_PLC: {
+        const { ok } = await requestPlcToken(state, env);
+        if (ok) {
+          session.set("requestedPlcToken", ok);
+        }
+        break;
+      }
+
+      case STAGES.ACTIVATE_DEST:
+      case STAGES.DEACTIVATE_ORIGIN:
+      case STAGES.MIGRATE_PLC: {
+        const { ok } = await validatePlcToken(state, data, env);
+        if (ok) {
+          session.set("destActivated", ok);
+          session.set("originDeactivated", ok);
+          session.set("migratedPlc", ok);
+        }
+        break;
+      }
     }
   }
-}
 
   return state;
 };
