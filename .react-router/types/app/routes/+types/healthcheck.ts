36935ab1a331ac85edcb7894ea7b7aa6dfd150bd
Andrés Ignacio Torres <dev@aitorres.com>
2026-04-22 23:42:27 -0700

    Add dedicated healthcheck to app (#107)

--- .react-router/types/app/routes/+types/healthcheck.ts
+++ .react-router/types/app/routes/+types/healthcheck.ts
@@ -2,10 +2,10 @@
 
 import type { GetInfo, GetAnnotations } from "react-router/internal";
 
-type Module = typeof import("../success.js")
+type Module = typeof import("../healthcheck.js")
 
 type Info = GetInfo<{
-  file: "routes/success.tsx",
+  file: "routes/healthcheck.ts",
   module: Module
 }>
 
@@ -13,8 +13,8 @@ type Matches = [{
   id: "root";
   module: typeof import("../../root.js");
 }, {
-  id: "routes/success";
-  module: typeof import("../success.js");
+  id: "routes/healthcheck";
+  module: typeof import("../healthcheck.js");
 }];
 
 type Annotations = GetAnnotations<Info & { module: Module, matches: Matches }>;
