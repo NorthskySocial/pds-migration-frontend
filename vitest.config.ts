2b32f8b083dd03d7bb6f861855bb7202f95b842d
Andrés Ignacio Torres <dev@aitorres.com>
2026-04-22 23:32:00 -0700

    Use vitest for unit tests

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vitest/config";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "~": resolve(__dirname, "./app"),
+    },
+  },
+  test: {
+    globals: true,
+    environment: "node",
+    include: ["test/unit/**/*.test.ts"],
+  },
+});
