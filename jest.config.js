8c960417d75190773db28dc6363a1eaa40aa0f7e
polaris <andres@northskysocial.com>
2026-08-04 22:03:19 -0700

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,18 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
+  testMatch: ["<rootDir>/test/e2e.test.ts"],
   transform: {
-    "^.+.tsx?$": ["ts-jest", {}],
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/tsconfig.tests.json",
+        useESM: true,
+      },
+    ],
+  },
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  moduleNameMapper: {
+    "^~/(.*)$": "<rootDir>/app/$1",
   },
   preset: "jest-puppeteer",
 };
