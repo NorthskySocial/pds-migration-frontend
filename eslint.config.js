d400f95d90c3c2344593f60fc60570751b475c15
polaris <andres@northskysocial.com>
2026-08-21 17:15:42 -0700

    Set up precommit hook and Github Actions checks for linting and format

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: [".react-router", "build", "coverage", "dist"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
@@ -25,5 +25,5 @@ export default tseslint.config(
       ],
       "react-refresh/only-export-components": ["off"],
     },
-  }
+  },
 );
